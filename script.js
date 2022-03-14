a5d56024bbdf31357bbb5db04d2ca08ce298976b
Mario Ferser <mefernandez@unimagdalena.edu.co>
2022-03-14 12:09:54 -0500

    I added project links and code

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 /*===== Menu =====*/
 
 ((d) => {
-  const $btnMenu = d.querySelector('.menu-btn'),
+	const $btnMenu = d.querySelector('.menu-btn'),
   $menu = d.querySelector('.menu');
-
+	
   $btnMenu.addEventListener('click', (e) => {
-    $btnMenu.firstElementChild.classList.toggle('none');
+		$btnMenu.firstElementChild.classList.toggle('none');
     $btnMenu.lastElementChild.classList.toggle('none');
     $menu.classList.toggle('is-active');
   });
-
+	
   d.addEventListener('click', (e) => {
-    if (!e.target.matches('.menu a')) return false;
-
+		if (!e.target.matches('.menu a')) return false;
+		
     $btnMenu.firstElementChild.classList.remove('none');
     $btnMenu.lastElementChild.classList.add('none');
     $menu.classList.remove('is-active');
   });
 })(document);
 
+/*===== Form =====*/
 
 const $form = document.querySelector('#form')
 
