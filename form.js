feb7627bffc5a4d84fbfb5bb5e9d9d1b2f5044f8
Mario Ferser <mefernandez@unimagdalena.edu.co>
2022-04-16 14:39:15 -0500

    I updated the modal and loader of the form

--- form.js
+++ form.js
@@ -4,13 +4,15 @@ const d = document;
 
 export default function contactFormSubmit(){
   const $form = d.querySelector(".contact__form"),
+  $response = d.querySelector(".form__response-text"),
   $spinner = d.querySelector(".contact__form-spinner"),
-  $response = d.querySelector(".contact__form-response");
-
+  $text = d.querySelector(".form__response-text");
 
 $form.addEventListener("submit",(e) => {
   e.preventDefault();
+  location.hash = "#thank";
   $spinner.classList.remove("none");
+
   fetch("https://formsubmit.co/ajax/9b84a8f87d2526fdc839483f1ab8409b",{
     method:"POST",
     body:new FormData(e.target),
@@ -18,16 +20,17 @@ $form.addEventListener("submit",(e) => {
   .then(res=> res.ok ? res.json():Promise.reject(res))
   .then(json => {
     console.log(json);
-    location.hash = "#thank";
     $form.reset();
   })
   .catch(err=> {
     console.log(err);
     let message = err.statusText || "Se origíno un error al enviar, intenta nuevamente :)"
-    $response.querySelector("h3").innerHTML = `Error${err.status}: ${message}`;
+    $response.querySelector(".form__response-text").innerHTML = `Error${err.status}: ${message}`;
   }).finally(()=> {
     $spinner.classList.add("none");
+    $text.classList.remove("none");
     setTimeout(()=>{
+
       location.hash = "#close"
     },3000);
   })
