9d3a0ab0a9e0ba2f212d4e3008106c142b5c9a3b
Mario Ferser <mefernandez@unimagdalena.edu.co>
2022-04-16 14:39:15 -0500

    I updated the modal and loader of the from

--- form.js
+++ form.js
@@ -4,13 +4,16 @@ const d = document;
 
 export default function contactFormSubmit(){
   const $form = d.querySelector(".contact__form"),
+  $response = d.querySelector(".form__response-text"),
   $spinner = d.querySelector(".contact__form-spinner"),
-  $response = d.querySelector(".contact__form-response");
-
+  $text = d.querySelector(".form__response-text");
 
 $form.addEventListener("submit",(e) => {
   e.preventDefault();
+  location.hash = "#thank";
   $spinner.classList.remove("none");
+  $text.classList.add("none")
+
   fetch("https://formsubmit.co/ajax/9b84a8f87d2526fdc839483f1ab8409b",{
     method:"POST",
     body:new FormData(e.target),
@@ -18,7 +21,6 @@ $form.addEventListener("submit",(e) => {
   .then(res=> res.ok ? res.json():Promise.reject(res))
   .then(json => {
     console.log(json);
-    location.hash = "#thank";
     $form.reset();
   })
   .catch(err=> {
@@ -27,6 +29,7 @@ $form.addEventListener("submit",(e) => {
     $response.querySelector("h3").innerHTML = `Error${err.status}: ${message}`;
   }).finally(()=> {
     $spinner.classList.add("none");
+    $text.classList.remove("none");
     setTimeout(()=>{
       location.hash = "#close"
     },3000);
